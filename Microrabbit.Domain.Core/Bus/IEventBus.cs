8fed237b6b340d4e1e9caea1e0fcdb8a600a7b99
addisalem tafere <addisalem12@gmail.com>
2021-12-20 06:06:51 -0500

    Generic Event handler added

---
+++ Microrabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microrabbit.Domain.Core.Commands;
+using Microrabbit.Domain.Core.Events;
+
+namespace Microrabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>;
+    }
+}
---
+++ Microrabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microrabbit.Domain.Core.Events;
+
+namespace Microrabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        private DateTime TimeStamp { get; set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
